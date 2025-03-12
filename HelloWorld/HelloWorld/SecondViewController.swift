9a20f62346e540fd3c0342472550773aa9793656
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-03-12 14:17:01 +0900

    SecondViewController와 ViewController 간의 delegate 패턴 구현: 메시지 전달 기능 추가

--- HelloWorld/HelloWorld/SecondViewController.swift
+++ HelloWorld/HelloWorld/SecondViewController.swift
@@ -7,54 +7,60 @@
 
 import UIKit
 
+protocol SecondViewControllerDelegate: AnyObject {
+  func didDismissSecondViewController(message: String)
+}
+
 class SecondViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        print("1 ViewController.viewDidLoad()")
-        
-        setupUI()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-      super.viewWillAppear(animated)
-      print("2 ViewController.viewWillAppear()")
-    }
+  weak var delegate: SecondViewControllerDelegate?
 
-    override func viewIsAppearing(_ animated: Bool) {
-      super.viewIsAppearing(animated)
-      print("3 ViewController.viewIsAppearing()")
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    print("01 SecondViewController.viewDidLoad()")
+    setupUI()
+  }
 
-    override func viewDidAppear(_ animated: Bool) {
-      super.viewDidAppear(animated)
-      print("4 ViewController.viewDidAppear()")
-    }
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("02 SecondViewController.viewWillAppear()")
+  }
 
-    override func viewWillDisappear(_ animated: Bool) {
-      super.viewWillDisappear(animated)
-      print("5 ViewController.viewWillDisappear()")
-    }
+  override func viewIsAppearing(_ animated: Bool) {
+    super.viewIsAppearing(animated)
+    print("03 SecondViewController.viewIsAppearing()")
+  }
 
-    override func viewDidDisappear(_ animated: Bool) {
-      super.viewDidDisappear(animated)
-      print("6 ViewController.viewDidDisappear()")
-    }
-    
-    func setupUI() {
-        print("7 ViewController.setupUI()")
-        // SecondViewController의 배경색을 노란색으로 결정
-        self.view.backgroundColor = .yellow
-        
-        let label = UILabel()
-        label.text = "Second View"
-        label.textAlignment = .center
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 24)
-        label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
-        self.view.addSubview(label)
-        
-    }
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    print("04 SecondViewController.viewDidAppear()")
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    print("05 SecondViewController.viewWillDisappear()")
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    print("06 SecondViewController.viewDidDisappear()")
+
+    // SecondViewController가 사라질 때 delegate에게 메시지를 전달
+    delegate?.didDismissSecondViewController(message: "Bye!")
+  }
+
+  func setupUI() {
+    print("07 SecondViewController.setupUI()")
+    // SecondViewController의 배경색을 노란색으로 설정
+    self.view.backgroundColor = .yellow
+
+    let label = UILabel()
+    label.text = "Second View"
+    label.textAlignment = .center
+    label.textColor = .black
+    label.font = UIFont.systemFont(ofSize: 24)
+    label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
+    self.view.addSubview(label)
+  }
+
 }
