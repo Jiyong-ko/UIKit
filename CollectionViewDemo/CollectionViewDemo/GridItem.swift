95b68bdf4b162b33ef1af897d2c1d466d13b3156
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-03-24 13:37:23 +0900

    그리드 컬렉션 뷰 셀 및 데이터 모델 추가, 컬렉션 뷰 구성 및 데이터 소스 설정

---
+++ CollectionViewDemo/CollectionViewDemo/GridItem.swift
@@ -0,0 +1,21 @@
+//
+//  GridItem.swift
+//  CollectionViewDemo
+//
+//  Created by NoelMacMini on 3/24/25.
+//
+import Foundation
+
+struct GridItem: Hashable {
+  let id = UUID()
+  let title: String
+  let imageSystemName: String
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+
+  static func == (lhs: GridItem, rhs: GridItem) -> Bool {
+    return lhs.id == rhs.id
+  }
+}
