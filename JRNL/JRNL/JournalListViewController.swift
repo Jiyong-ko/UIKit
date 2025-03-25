0daf03c33f9151623ad3b3dee5d51596ed8bac32
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-03-25 15:20:49 +0900

    Map뷰 추가, Detail TableViewController 추가, (journalCell 인터페이스 수정중)

--- JRNL/JRNL/JournalListViewController.swift
+++ JRNL/JRNL/JournalListViewController.swift
@@ -1,28 +1,32 @@
 //
-//  JournalListViewController.swift
+//  ViewController.swift
 //  JRNL
 //
-//  Created by NoelMacMini on 3/25/25.
+//  Created by Jungman Bae on 3/25/25.
 //
- 
- import UIKit
- 
- class JournalListViewController: UIViewController {
- 
-   @IBOutlet weak var tableView: UITableView!
- 
-   override func viewDidLoad() {
-     super.viewDidLoad()
-   }
- }
- 
- extension JournalListViewController: UITableViewDataSource {
-   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     return 10
-   }
- 
-   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
-   }
- }
- 
+
+import UIKit
+
+class JournalListViewController: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
+    print("unwindNewEntryCancel")
+  }
+}
+
+extension JournalListViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+  }
+}
+
