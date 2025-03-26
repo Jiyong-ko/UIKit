a68b7b7d49d31269fce53213db88afe15436a881
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-03-26 13:10:50 +0900

    Add AddJournalEntryViewController 및 스토리보드 연결

---
+++ JRNL/JRNL/AddJournalEntryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddJournalEntryViewController.swift
+//  JRNL
+//
+//  Created by NoelMacMini on 3/26/25.
+//
+
+import UIKit
+
+class AddJournalEntryViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    var newJournalEntry: JournalEntry?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("prepare")
+        let title = titleTextField.text ?? ""
+        let body = bodyTextView.text ?? ""
+        let photo = photoImageView.image
+        let rating = 3
+        newJournalEntry = JournalEntry(rating: rating, title: title, body: body, photo: photo)
+    }
+    
+
+}
